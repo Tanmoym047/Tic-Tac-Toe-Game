2173f658006a7b8059e73246be9cc441c93e6312
Tanmoym047 <tanmoym047@gmail.com>
2022-11-13 15:46:42 +0600

    Update main.c

--- main.c
+++ main.c
@@ -20,7 +20,14 @@ void PrintWinner(char);
 int main()
 {
     char winner = ' ';
+
     ResetBoard();
+
+    while(winner == ' ' && CheckFreeSpace() != 0)
+    {
+        PrintBoard();
+    }
+    
 
 
     return 0;
@@ -28,14 +35,38 @@ int main()
 
 void ResetBoard()
 {
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            board[i][j] = ' ';
+        }
+    }
 
 }
 void PrintBoard()
 {
-
+    printf("\n %c | %c | %c ", board[0][0], board[0][1], board[0][2]);
+    printf("\n---|---|---");
+    printf("\n %c | %c | %c ", board[1][0], board[1][1], board[1][2]);
+    printf("\n---|---|---");
+    printf("\n %c | %c | %c ", board[2][0], board[2][1], board[2][2]);
+    
 }
 int CheckFreeSpace()
 {
+    int FreeSpace = 9;
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            if(board[i][j] != ' ')
+            {
+                FreeSpace --;
+            }
+        }
+    }
+    return FreeSpace;
 
 }
 void PlayerMove()
