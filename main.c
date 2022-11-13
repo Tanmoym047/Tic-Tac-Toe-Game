79bf6939d7a0e8970396d73bacc0e9ba856cec3e
Tanmoym047 <tanmoym047@gmail.com>
2022-11-13 13:28:30 +0600

    Update main.c

--- main.c
+++ main.c
@@ -2,3 +2,56 @@
 #include <time.h>
 #include <ctype.h>
 #include <stdlib.h>
+
+char board[3][3];
+const char PLAYER = 'X';
+const char COMPUTER = 'O';
+
+
+void ResetBoard();
+void PrintBoard();
+int CheckFreeSpace();
+void PlayerMove();
+void ComputerMove();
+char CheckWinner();
+void PrintWinner(char);
+
+
+int main()
+{
+    char winner = ' ';
+    ResetBoard();
+
+
+    return 0;
+}
+
+void ResetBoard()
+{
+
+}
+void PrintBoard()
+{
+
+}
+int CheckFreeSpace()
+{
+
+}
+void PlayerMove()
+{
+
+}
+void ComputerMove()
+{
+
+}
+char CheckWinner()
+{
+
+}
+void PrintWinner(char)
+{
+
+
+}
