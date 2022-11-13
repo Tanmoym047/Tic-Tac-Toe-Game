3e1575628cb979c61bd6aea680853f3382bf0527
Tanmoym047 <tanmoym047@gmail.com>
2022-11-13 19:25:07 +0600

    Update main.c

--- main.c
+++ main.c
@@ -26,6 +26,19 @@ int main()
         PrintBoard();
 
         PlayerMove();
+        winner = CheckWinner();
+        if(winner != ' ' || CheckFreeSpace() == 0)
+        {
+            break;
+        }
+
+        ComputerMove();
+        winner = CheckWinner();
+        if(winner != ' ' || CheckFreeSpace() == 0)
+        {
+            break;
+        }
+
     }
 
     return 0;
@@ -91,9 +104,41 @@ void PlayerMove()
 }
 void ComputerMove()
 {
+    //create a seed
+    srand(time(0));
+    
 }
 char CheckWinner()
 {
+    //check rows
+    for(int i = 0; i < 3; i++)
+    {
+        if(board[i][0] = board[i][1] && board[i][0] = board[i][2])
+        {
+            return board[i][0];
+        }
+    }
+    //check columns
+    for(int i = 0; i < 3; i++)
+    {
+        if(board[0][i] = board[1][i] && board[0][i] = board[2][i])
+        {
+            return board[0][i];
+        }
+    }
+    //check diagonal 1
+    if(board[0][0] = board[1][1] && board[0][0] = board[2][2])
+        {
+            return board[0][0];
+        }
+    //check diagonal 2
+    if(board[0][2] = board[1][1] && board[0][2] = board[2][0])
+        {
+            return board[0][2];
+        }
+    
+    return ' '; 
+
 }
 void PrintWinner(char)
 {
